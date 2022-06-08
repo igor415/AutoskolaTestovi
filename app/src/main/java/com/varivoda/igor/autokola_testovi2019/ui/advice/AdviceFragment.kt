c0886608a712d14ab6eff428f66a7b47362b8e3f
igor415 <igor.varivoda19@gmail.com>
2022-06-08 21:43:20 +0200

    adaptive admob banner

--- app/src/main/java/com/varivoda/igor/autokola_testovi2019/ui/advice/AdviceFragment.kt
+++ app/src/main/java/com/varivoda/igor/autokola_testovi2019/ui/advice/AdviceFragment.kt
@@ -1,13 +1,17 @@
 package com.varivoda.igor.autokola_testovi2019.ui.advice
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.DisplayMetrics
+import android.view.Display
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.varivoda.igor.autokola_testovi2019.R
 import com.varivoda.igor.autokola_testovi2019.databinding.FragmentAdviceBinding
 
@@ -15,6 +19,7 @@ import com.varivoda.igor.autokola_testovi2019.databinding.FragmentAdviceBinding
 class AdviceFragment : Fragment() {
 
     private lateinit var binding: FragmentAdviceBinding
+    private lateinit var adView: AdView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +35,29 @@ class AdviceFragment : Fragment() {
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner){
             requireActivity().finish()
         }
+        adView = AdView(requireContext())
+        binding.adViewContainer.addView(adView)
+        adView.adUnitId = "ca-app-pub-3940256099942544/6300978111"
+        loadBanner()
+    }
+
+    private fun loadBanner() {
         val adRequest = AdRequest.Builder().build()
-        binding.adView.loadAd(adRequest)
+        val adSize = getAdSize()
+        adView.adSize = adSize
+        adView.loadAd(adRequest)
+    }
+
+    private fun getAdSize(): AdSize? {
+        val display: Display = requireActivity().windowManager.getDefaultDisplay()
+        val outMetrics = DisplayMetrics()
+        display.getMetrics(outMetrics)
+        val widthPixels = outMetrics.widthPixels.toFloat()
+        val density = outMetrics.density
+
+        val adWidth = (widthPixels / density).toInt()
+
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(requireContext(), adWidth)
     }
 
 }
